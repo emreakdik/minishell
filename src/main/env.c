20b59d8afa517fbcb982fb8d0a02c2caf2489efb
emreakdik <yemre.akdik@gmail.com>
2023-12-05 15:49:08 +0300

    get_title_from_env: some leak fixes

--- src/main/env.c
+++ src/main/env.c
@@ -6,7 +6,7 @@
 /*   By: emre <emre@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 18:29:16 by aaybaz            #+#    #+#             */
-/*   Updated: 2023/12/05 15:30:15 by emre             ###   ########.fr       */
+/*   Updated: 2023/12/05 15:46:44 by emre             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,27 @@
 
 void get_title_from_env(t_shell **shell)
 {
-	char *user;
-	char *pwd;
-	char *title;
-	char *temp;
+    char *user;
+    char *pwd;
+    char *title;
+    char *temp;
 
-	if ((*shell)->title)
-		free((*shell)->title);
-	user = get_env((*shell)->env, "USER");
-	pwd = get_env((*shell)->env, "PWD");
-	title = ft_strjoin("\033[1;36m", user);
-	free(user);
-	temp = ft_strjoin(title, "@");
-	free(title);
-	title = temp;
-	temp = ft_strjoin(title, pwd);
-	free(title);
-	title = temp;
-	temp = ft_strjoin(title, "$ \033[0m");
-	free(title);
-	(*shell)->title = temp;
-	free(pwd);
+    if ((*shell)->title)
+        free((*shell)->title);
+    user = get_env((*shell)->env, "USER");
+    pwd = get_env((*shell)->env, "PWD");
+    title = ft_strjoin("\033[1;36m", user);
+    free(user);
+    temp = ft_strjoin(title, "@");
+    free(title);
+    title = temp;
+    temp = ft_strjoin(title, pwd);
+    free(title);
+    free(pwd);
+    title = temp;
+    temp = ft_strjoin(title, "$ \033[0m");
+    free(title);
+    (*shell)->title = temp;
 }
 
 void free_str(char **str)
