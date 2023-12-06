39d5abf9474a91ce669f7a16ed5169679ec104df
emreakdik <yemre.akdik@gmail.com>
2023-12-06 20:35:55 +0300

    create_file: variables changed

--- src/files/create_files.c
+++ src/files/create_files.c
@@ -6,7 +6,7 @@
 /*   By: emre <emre@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 14:20:50 by ealbayra          #+#    #+#             */
-/*   Updated: 2023/12/06 20:25:59 by emre             ###   ########.fr       */
+/*   Updated: 2023/12/06 20:34:54 by emre             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,32 +41,32 @@ void	other_out_filesme(t_parse *parse)
 		free(pwd1);
 }
 
-void	other_text_create_me(t_parse *m_parse)
+void	other_text_create_me(t_parse *current_parse)
 {
 	t_parse	*n_parse;
 	int		i;
 	int		j;
 
-	n_parse = m_parse->next;
+	n_parse = current_parse->next;
 	i = 0;
-	while (m_parse->text[i])
+	while (current_parse->text[i])
 		i++;
 	while (n_parse->next && n_parse->type != 2)
 	{
 		j = 1;
 		while (n_parse->text[j])
 		{
-			m_parse->text[i] = ft_strdup(n_parse->text[j]);
+			current_parse->text[i] = ft_strdup(n_parse->text[j]);
 			i++;
 			j++;
 		}
 		n_parse = n_parse->next;
 	}
-	m_parse->text[i] = NULL;
-	other_out_filesme(m_parse);
+	current_parse->text[i] = NULL;
+	other_out_filesme(current_parse);
 }
 
-void	create_out_files_me(t_parse *m_parse, t_parse *prev_parse)
+void	create_out_files_me(t_parse *current_parse, t_parse *first_parse)
 {
 	char	str[256];
 	char	*pwd;
@@ -74,39 +74,41 @@ void	create_out_files_me(t_parse *m_parse, t_parse *prev_parse)
 	char	*temp;
 
 	getcwd(str, 256);
-	m_next = m_parse->next;
+	m_next = current_parse->next;
 	if (m_next->type == 3 || m_next->type == 4)
-		return (other_text_create_me(m_parse));
+		return (other_text_create_me(current_parse));
 	pwd = ft_strjoin(str, "/");
 	temp = ft_strjoin(pwd, m_next->text[0]);
 	free(pwd);
 	pwd = temp;
-	if (m_parse->type == 4)
+	if (current_parse->type == 4)
 		m_next->fd = open(pwd, O_CREAT | O_RDWR | O_APPEND, 0777);
-	else if (m_parse->type == 3)
+	else if (current_parse->type == 3)
 		m_next->fd = open(pwd, O_CREAT | O_RDWR | O_TRUNC, 0777);
-	if (m_parse->cmd)
-		m_parse->outfile = m_next->fd;
-	else if (prev_parse->cmd)
-		prev_parse->outfile = m_next->fd;
+	if (current_parse->cmd)
+		current_parse->outfile = m_next->fd;
+	else if (first_parse->cmd)
+		first_parse->outfile = m_next->fd;
 	if (pwd)
 		free(pwd);
 }
 
 int	create_files_m(t_shell *m_shell)
 {
-	t_parse	*parse;
+	t_parse	*current_parse;
+	t_parse  *first_parse;
 	int		i;
 
 	i = 1;
-	parse = m_shell->parse;
-	while (parse)
+	current_parse = m_shell->parse;
+	first_parse = m_shell->parse;
+	while (current_parse)
 	{
-		if (parse->type == 3 || parse->type == 4)
-			create_out_files_me(parse, m_shell->parse);
-		else if (parse->type == 5)
-			i = create_in_files_me(parse);
-		parse = parse->next;
+		if (current_parse->type == 3 || current_parse->type == 4)
+			create_out_files_me(current_parse, first_parse);
+		else if (current_parse->type == 5)
+			i = create_in_files_me(current_parse);
+		current_parse = current_parse->next;
 	}
 	return (i);
 }
